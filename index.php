4b8c907cdea5553b834f976b67e774a9fb88d071
code00x <146408559+code00x@users.noreply.github.com>
2023-10-20 23:30:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+ 
+<!DOCTYPE html> 
+<!-- this is where we enter our username and password to login --> 
+<html> 
+<head> 
+<title>LOGIN</title> 
+<link rel="stylesheet" type="text/css" href="style.css"> 
+</head> 
+<body> 
+<form action="login.php" method="post"> 
+<h2>LOGIN FORM</h2> 
+</p> 
+<label> User Name : </label> 
+<input type="text" name="uname" placeholder="enter your user name " /> 
+<label>Password : </label> 
+<input type="password" name="pwd" placeholder="password" /> 
+<button name="submit">LOGIN</button> 
+</form> 
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body> 
+</html> 
